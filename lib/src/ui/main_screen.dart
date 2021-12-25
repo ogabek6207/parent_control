61def4cda87524773159496089e18717abf40eed
KOSSHOH <shahboz.turonov98@gmail.com>
2021-12-25 12:15:01 +0500

    main screen

---
+++ lib/src/ui/main_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: 200,
+        width: 200,
+        color: Colors.blueAccent,
+      ),
+    );
+  }
+}
