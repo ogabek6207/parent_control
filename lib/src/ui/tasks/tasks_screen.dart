d89b81b92db5e981c6c3c3fc0f0237e2edd0955b
Ogabek <ogabekabdijabborov@gmail.com>
2022-01-05 10:46:25 +0500

    tasks_screen.dart....

--- lib/src/ui/tasks/tasks_screen.dart
+++ lib/src/ui/tasks/tasks_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:parent_control/src/app%20theme/app_thema.dart';
+import 'package:parent_control/src/utils/utils.dart';
 
 class TasksScreen extends StatefulWidget {
   const TasksScreen({Key? key}) : super(key: key);
@@ -11,10 +12,31 @@ class TasksScreen extends StatefulWidget {
 class _TasksScreenState extends State<TasksScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-backgroundColor: AppTheme.blue,
-      appBar: AppBar(
-
+    double h = Utils.windowHeight(context);
+    double w = Utils.windowWidth(context);
+    double o = (h + w) / 2;
+    return Scaffold(
+      backgroundColor: AppTheme.blue,
+      body: Column(
+        children: [
+          Container(
+            height: 48 * h,
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.only(top: 48 * h),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 24 * w,
+                ),
+                Icon(
+                  Icons.arrow_back_ios,
+                  size: 24,
+                  color: AppTheme.white,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
